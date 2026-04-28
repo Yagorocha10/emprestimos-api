fdd70902c993d0b072764b6b1fb169ef10a03186
Yagorocha10 <yagodasilvarocha30@gmail.com>
2026-04-27 21:43:24 -0300

    feat: commit inicial - projeto emprestimos api

---
+++ src/main/java/com/yago/emprestimos_api/EmprestimosApiApplication.java
@@ -0,0 +1,13 @@
+package com.yago.emprestimos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yago/emprestimos_api/EmprestimosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yago.emprestimos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
